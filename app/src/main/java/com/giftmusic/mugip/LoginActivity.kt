1e31a1b8e45ec90cd2ba51284bd8b96294f4ec9d
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-06-28 17:17:00 +0900

    구글 로그인 추가

--- app/src/main/java/com/giftmusic/mugip/LoginActivity.kt
+++ app/src/main/java/com/giftmusic/mugip/LoginActivity.kt
@@ -1,11 +1,17 @@
 package com.giftmusic.mugip
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.nhn.android.naverlogin.OAuthLogin
 import com.nhn.android.naverlogin.OAuthLoginHandler
 
@@ -21,6 +27,7 @@ class LoginActivity : AppCompatActivity() {
         
         // 네이버 로그인
         naverLoginButton.setOnClickListener(LoginButtonListener())
+        googleLoginButton.setOnClickListener(LoginButtonListener())
     }
 
     inner class LoginButtonListener : View.OnClickListener{
@@ -55,9 +62,41 @@ class LoginActivity : AppCompatActivity() {
                     mOauthLoginModule.startOauthLoginActivity(this@LoginActivity, mOAuthLoginHandler)
                 }
 
-                R.id.btn_login_google -> {}
+                R.id.btn_login_google -> {
+                    startActivityForResult(googleSignInIntent, RESULT_CODE)
+                }
                 R.id.btn_login_email -> {}
                 R.id.btn_signup -> {}
+            }
+        }
+    }
+
+    private val googleSignInIntent by lazy {
+        val googleSignInOption = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail().requestProfile().build()
+        GoogleSignIn.getClient(applicationContext, googleSignInOption).signInIntent
+    }
+
+    companion object{
+        const val RESULT_CODE = 10
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == RESULT_CODE) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+
+            result?.let {
+                if (it.isSuccess) {
+                    it.signInAccount?.displayName //이름
+                    it.signInAccount?.email //이메일
+                    Log.e("Value", it.signInAccount?.email!!)
+                    // 기타 등등
+                } else  {
+                    Log.e("Value", "error")
+                    // 에러 처리
+                }
             }
         }
     }
