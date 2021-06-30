0d719ddbe6a1f4f9378f455184637efb9084c1fd
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-06-30 20:47:03 +0900

    네이버 로그인 API 제거

--- app/src/main/java/com/giftmusic/mugip/LoginActivity.kt
+++ app/src/main/java/com/giftmusic/mugip/LoginActivity.kt
@@ -3,63 +3,41 @@ package com.giftmusic.mugip
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.nhn.android.naverlogin.OAuthLogin
-import com.nhn.android.naverlogin.OAuthLoginHandler
+import java.security.MessageDigest
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val naverLoginButton : Button = findViewById(R.id.btn_login_naver)
+        hashKey()
+        val kakaoLoginButton : Button = findViewById(R.id.btn_login_kakao)
         val googleLoginButton : Button = findViewById(R.id.btn_login_google)
         val emailLoginButton : Button = findViewById(R.id.btn_login_email)
         val signUpButton : Button = findViewById(R.id.btn_signup)
         
         // 네이버 로그인
-        naverLoginButton.setOnClickListener(LoginButtonListener())
+        kakaoLoginButton.setOnClickListener(LoginButtonListener())
         googleLoginButton.setOnClickListener(LoginButtonListener())
     }
 
     inner class LoginButtonListener : View.OnClickListener{
         override fun onClick(v: View?) {
             when(v?.id){
-                R.id.btn_login_naver -> {
-                    val mOauthLoginModule = OAuthLogin.getInstance()
-                    mOauthLoginModule.init(
-                        applicationContext,
-                        BuildConfig.Naver_Client_ID,
-                        BuildConfig.Naver_Client_Secret,
-                        BuildConfig.Naver_Client_Name
-                    )
+                R.id.btn_login_kakao -> {
 
-
-                    @SuppressLint("HandlerLeak")
-                    val mOAuthLoginHandler : OAuthLoginHandler = object : OAuthLoginHandler(){
-                        override fun run(success: Boolean){
-                            if(success){
-
-                            } else {
-                                val errorCode: String = mOauthLoginModule.getLastErrorCode(applicationContext).code
-                                val errorDesc = mOauthLoginModule.getLastErrorDesc(applicationContext)
-
-                                Toast.makeText(
-                                    baseContext, "errorCode:" + errorCode
-                                            + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                        }
-                    }
-                    mOauthLoginModule.startOauthLoginActivity(this@LoginActivity, mOAuthLoginHandler)
                 }
 
                 R.id.btn_login_google -> {
@@ -98,6 +76,19 @@ class LoginActivity : AppCompatActivity() {
                     // 에러 처리
                 }
             }
+        }
+    }
+
+    private fun hashKey() {
+        try {
+            val pkinfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+            for (signature in pkinfo.signatures) {
+                val messageDigest: MessageDigest = MessageDigest.getInstance("SHA")
+                messageDigest.update(signature.toByteArray())
+                val result: String = String(Base64.encode(messageDigest.digest(), 0))
+                Log.d("해시", result)
+            }
+        } catch (e: Exception) {
         }
     }
 }
