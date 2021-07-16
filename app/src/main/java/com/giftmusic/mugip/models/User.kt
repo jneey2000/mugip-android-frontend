7c9b320a3c034ab78de49798ca6a86cbc3065888
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-07-16 10:50:03 +0900

    다른 유저의 정보를 표현하기 위한  model 추가

---
+++ app/src/main/java/com/giftmusic/mugip/models/User.kt
@@ -0,0 +1,8 @@
+package com.giftmusic.mugip.models
+
+import com.google.android.gms.maps.model.LatLng
+
+class OtherUser(imageUrl: String, location: LatLng) {
+    val imageUrl : String = imageUrl
+    val location : LatLng = location
+}
