7bb4992a3db5790cdb67975049ac9f0814cdda41
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-11-09 15:05:04 +0900

    업로드 날짜 표시를 위한 코드 추가

--- app/src/main/java/com/giftmusic/mugip/models/MusicItem.kt
+++ app/src/main/java/com/giftmusic/mugip/models/MusicItem.kt
@@ -1,8 +1,11 @@
 package com.giftmusic.mugip.models
 
+import java.time.LocalDateTime
+
 data class MusicItem(
     val musicThumbnailURL : String,
     val musicTitle : String,
     val artist : String,
-    val category: String
+    val category: String,
+    val createdAt: LocalDateTime
 )
